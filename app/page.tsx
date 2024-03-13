91bdf4dd543b87ae672354a7114b2f09697c16d1
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-13 12:51:54 +0100

    designs

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,30 @@ import {FC} from "react";
 
 const Page: FC = () => {
     return (
-        <div className={"text-center pt-20"}>
-            <h1 className={"text-9xl text-normal font-thin"}>Atelier B²</h1>
-            <h1 className={"text-9xl text-normal font-light"}>Atelier B²</h1>
-            <h1 className={"text-9xl text-normal font-normal"}>Atelier B²</h1>
-        </div>
-    );
+        <>
+            <div className={"text-center py-20"}>
+
+                <h1 className={"text-5xl  text-normal font-thin"}>Atelier B²</h1>
+                <h1 className={"text-5xl text-normal font-light"}>Atelier B²</h1>
+                <h1 className={"text-5xl  text-normal font-normal"}>Atelier B²</h1>
+            </div>
+            <div className={"text-center py-20"}>
+                <h1 className={"text-5xl  text-normal font-thin"}>Atelier B²</h1>
+                <h1 className={"text-5xl -mt-16 text-normal font-thin"}>Atelier B²</h1>
+                <h1 className={"text-5xl -mt-16 text-normal font-thin"}>Atelier B²</h1>
+
+
+
+            </div>
+            <div className={"text-center my-20"}>
+                <h1 className={"text-5xl  text-normal font-thin"}>Atelier B²</h1>
+                <h1 className={"text-5xl ml-6 -mt-16 text-normal font-light"}>Atelier B²</h1>
+                <h1 className={"text-5xl ml-10 -mt-16 text-normal font-normal"}>Atelier B²</h1>
+            </div>
+        </>
+
+    )
+        ;
 }
 
 export default Page;
